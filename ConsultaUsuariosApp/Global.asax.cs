425a8539c3cacf370029a418a56fdd77f3b66331
carlos29 thomas <carlos29thomas@hotmail.com>
2025-10-12 09:01:59 -0400

    cambios de soluciones

--- ConsultaUsuariosApp/Global.asax.cs
+++ ConsultaUsuariosApp/Global.asax.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿// Añade esta línea al principio para poder usar tu nuevo inicializador
+
+using System.Data.Entity; // Asegúrate de que esta línea también exista
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -16,6 +15,8 @@ namespace ConsultaUsuariosApp
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+          
         }
     }
 }
